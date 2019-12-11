6f0e02da0dfc21f30309e287aa3ec4e1a90a7319
Kody Quintana <Quintana.Kody@gmail.com>
2019-12-11 14:31:34 -0700

    Add untracked files

---
+++ home/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse #for simple responses
+
+from . models import ResourceLink
+
+# Create your views here.
+
+def homepage(request):
+    #return HttpResponse("literal html can go here")
+    resource_links = ResourceLink.objects.all().order_by('title')
+    return render(request, 'home/homepage.html', {'resource_links':resource_links})
+
+def resource_info(request, title):
+    this_resource = ResourceLink.objects.get(slug=title)
+    return render(request, 'home/resource_detail.html', {'this_resource' : this_resource})
+    #return HttpResponse('resource info' + str(title))
