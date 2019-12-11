0e4c0f6a41f7ac37614e3bad462b5fa8fc09abc9
Kody Quintana <Quintana.Kody@gmail.com>
2019-12-11 14:54:01 -0700

    Change installed path for quiz

--- training_site/settings.py
+++ training_site/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users',
     'home',#homepage app
-    'django_quiz/quiz',
+    'quiz',
     'multichoice',
     'true_false',
     'essay',
